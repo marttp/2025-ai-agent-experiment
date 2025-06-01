ca99a90b331ac2954be25e992f26c3c553799683
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-06-01 13:18:24 +0900

    feat: implement Redis caching for assistant agent state persistence

--- 20250601-autogen/agent/chat_agent.py
+++ 20250601-autogen/agent/chat_agent.py
@@ -2,22 +2,19 @@ import os
 import asyncio
 from autogen_ext.models.openai import (
     OpenAIChatCompletionClient,
-    AzureOpenAIChatCompletionClient,
 )
 from autogen_core.models import (
     ModelInfo,
     ModelFamily,
-    SystemMessage,
-    UserMessage,
-    AssistantMessage,
 )
 from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.messages import TextMessage
 from autogen_core import CancellationToken
 import json
-# from autogen_ext.models.cache import CHAT_CACHE_VALUE_TYPE
-# from autogen_ext.cache_store.redis import RedisStore
-# import redis
+
+from autogen_ext.models.cache import CHAT_CACHE_VALUE_TYPE
+from autogen_ext.cache_store.redis import RedisStore
+import redis
 
 
 async def main():
@@ -40,6 +37,18 @@ async def main():
         system_message="You're a helpful personal assistant.",
     )
 
+    # if os.path.exists("assistant_agent.json"):
+    #     with open("assistant_agent.json", "r") as f:
+    #         state = json.load(f)
+    #         await assistant_agent.load_state(state)
+
+    redis_instance = redis.Redis()
+    cache_store = RedisStore[CHAT_CACHE_VALUE_TYPE](redis_instance)
+    state = cache_store.get("assistant_agent")
+    if state:
+        state = json.loads(state)
+        await assistant_agent.load_state(state)
+
     while True:
         user_message = input("User: ")
         if user_message == "exit":
@@ -52,6 +61,13 @@ async def main():
             cancellation_token=cancellation_token,
         )
         print(f"{response.chat_message.content}\n{response.chat_message.models_usage}")
+
+    # with open("assistant_agent.json", "w") as f:
+    #     state = await assistant_agent.save_state()
+    #     json.dump(state, f, indent=4)
+
+    state = await assistant_agent.save_state()
+    cache_store.set("assistant_agent", json.dumps(state))
 
 
 asyncio.run(main())

75ebce35add6a72dc4e230a21c7065a4ba4e5557
Thanaphoom Babparn <tp.babparn@gmail.com>
2025-06-01 12:55:26 +0900

    feat: implement basic chat interface for Gemma model using autogen_ext

---
+++ 20250601-autogen/chat/chat-gemma.py
@@ -0,0 +1,41 @@
+import os
+import asyncio
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_core.models import (
+    ModelInfo,
+    ModelFamily,
+    SystemMessage,
+    UserMessage,
+    AssistantMessage,
+)
+
+
+async def main():
+    model_client_ollama = OpenAIChatCompletionClient(
+        model="gemma3:12b",
+        base_url=os.environ["OLLAMA_BASE_URL"],
+        api_key="ollama",
+        model_info=ModelInfo(
+            vision=True,
+            function_calling=True,
+            json_output=True,
+            family=ModelFamily.UNKNOWN,
+            structured_output=True,
+        ),
+    )
+
+    messages = []
+    messages.append(SystemMessage(content="You're a helpful personal assistant"))
+
+    while True:
+        user_message = input("User: ")
+        if user_message == "exit":
+            break
+
+        messages.append(UserMessage(content=user_message, source="user"))
+        response = await model_client_ollama.create(messages=messages)
+        print(f"{response.content}\n{response.usage}")
+        messages.append(AssistantMessage(content=response.content, source="assistant"))
+
+
+asyncio.run(main())
